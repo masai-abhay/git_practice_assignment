011a44aa63f5de5aab4f1f00afec8415eef81688
masai-abhay <148424787+masai-abhay@users.noreply.github.com>
2023-10-21 00:50:54 +0530

    function for checking prime

---
+++ checkprime.js
@@ -0,0 +1,25 @@
+function isPrime(number) {
+    if (number = 1) {  
+      return false;
+    }
+    if (number === 2) {
+      return true;
+    }
+    for (let i = 2; i * i <= number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    } 
+    return true;
+  }
+
+  function PrintPrime() {
+    for (let i = 2; i = 20; i++) {
+      if (isPrime(i)) {
+        console.log(num);
+      }
+    }
+  }
+  
+  PrintPrime();
+//   here on this code line 2(ommoted < before =) & 17(ommoted < before =) & 19 (i should be printed instead of num) are intentionally written wrong
